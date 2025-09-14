0550a9a38eedccbc33a9877cfd85d8334b59e82e
BissyClassic <rachaelafolabi2021@gmail.com>
2025-09-14 13:24:03 +0100

    Add updated task_1

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -17,5 +17,17 @@ const teacher3: Teacher = {
   contract: false,
 };
 
-console.log(teacher3);
+interface Directors extends Teacher {
+  numberOfReports: number;
+}
 
+const director1: Directors = { 
+  firstName: 'John', 
+  lastName: 'Doe', 
+  location: 'London',
+  fullTimeEmployee: true, 
+  numberOfReports: 17,
+ };
+
+console.log(teacher3);
+console.log(director1);
