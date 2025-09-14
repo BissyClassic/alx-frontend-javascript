fe32fc4b7ccd983ddb96db64587f342ff67d90b0
BissyClassic <rachaelafolabi2021@gmail.com>
2025-09-14 15:26:28 +0100

    Add task_3 Ambient Namespaces implementation

---
+++ 0x04-TypeScript/task_3/js/interface.ts
@@ -0,0 +1,10 @@
+// Type alias for RowID
+export type RowID = number;
+
+// Interface for a row
+export interface RowElement {
+  firstName: string;
+  lastName: string;
+  age?: number; // optional
+}
+
